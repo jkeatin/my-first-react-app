aa1a5e1f4390aa0ca4f0615b211cf05298fefb8f
James Keating <jameskeating@adviserplus.com>
2020-10-30 16:34:31 +0000

    30.10.20 latest changes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
-
   state = {
     persons: [
-      { name: 'James', age: '31' }, 
-      { name: 'Jess', age: '29' }, 
-      { name: 'Bilbo', age: '7' }
-    ]
-  };
+      { name: 'James', age: 31 },
+      { name: 'Jess', age: 29 },
+      { name: 'Bilbo', age: 7 }
+    ],
+    otherState: 'some other value'
+  }
 
-  switchNameHandler = () => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'James Keating'
-    this.setState( {
+  switchNameHandler = (newName) => {
+    // DON'T DO THIS: this.state.persons[0].name = 'James Keating';
+    this.setState({
       persons: [
-        { name: 'James Keating', age: '31' }, 
-        { name: 'Jess', age: '29' }, 
-        { name: 'Bilbo', age: '7' }
+        { name: newName, age: 31 },
+        { name: 'Jess', age: 29 },
+        { name: 'Bilbo', age: 7 }
       ]
-    } )
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'James', age: 31 },
+        { name: event.target.value, age: 29 },
+        { name: 'Bilbo', age: 7 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am a React App.</h1>
+        <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Running and Cycling</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('James Keating')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'James Keating!!')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
+
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const person = ( props ) => {
+// This is a stateless (Dumb/Presentational) function component
+const person = (props) => {
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
