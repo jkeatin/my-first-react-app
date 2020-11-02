1ee5abccbaa3e7e3152227ddfa5502597fc9e94d
James Keating <j.keatin@gmail.com>
2020-11-02 17:35:28 +0000

    021120

--- src/App.js
+++ src/App.js
@@ -5,33 +5,47 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'James', age: 31 },
-      { name: 'Jess', age: 29 },
-      { name: 'Bilbo', age: 7 }
+      { id: '001', name: 'James', age: 31 },
+      { id: '002', name: 'Jess', age: 29 },
+      { id: '003', name: 'Bilbo', age: 7 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // DON'T DO THIS: this.state.persons[0].name = 'James Keating';
-    this.setState({
-      persons: [
-        { name: newName, age: 31 },
-        { name: 'Jess', age: 29 },
-        { name: 'Bilbo', age: 7 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object({},this.state.persons[personIndex]) // same as above but old way of doing it
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    person[personIndex] = person;
+
+    this.setState({person: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'James', age: 31 },
-        { name: event.target.value, age: 29 },
-        { name: 'Bilbo', age: 7 }
-      ]
-    })
-  }
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice(); // Create a copy of the persons array's current state in a new array
+    const persons = [...this.state.persons]; // this is an ES6 way of creating a copy of the persons array's current state in a new array
+    
+    /* ^^ You want to create a new copy of the state array so that you are editing an immutable version of the data, not one that has a reference point back to the original */
+  
+    persons.splice(personIndex, 1); // Remove one item at the index in the array that is passed in
+    this.setState({persons: persons}); // update the state of persons to the contents of the new spliced array
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState ({showPersons: !doesShow}) // this will toggle the bool value
+  };
 
   render() {
     const style = {
@@ -42,28 +56,34 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons === true) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => { //Takes the current state of the persons array and passes each item into this function which returns each element as a Person component
+            return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            })
+          }
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button 
           style={style}
-          onClick={() => this.switchNameHandler('James Keating')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'James Keating!!')}
-          changed={this.nameChangedHandler} >My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
-
   }
 }
 
